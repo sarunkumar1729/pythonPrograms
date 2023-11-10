e968b849360e3280b7b475aa1e11fc8893e52495
sarunkumar1729 <sarunkumarvv1993@gmail.com>
2023-11-10 14:37:16 +0530

    last commit

---
+++ notes/function_object.py
@@ -0,0 +1,12 @@
+# function object atributes 
+
+def fun(x):
+    """example function"""
+    return x
+
+print(fun)
+print(fun.__doc__)
+print(fun.__name__)
+print(fun.__annotations__)
+print(fun.__defaults__)
+print(fun.__code__)
